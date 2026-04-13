8f0f5eb38cefbb74f6235884fa0ecc927247a555
Enzo <digiovannienzo1@gmail.com>
2026-04-13 16:39:29 +0200

    New projects resources

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\AccountController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ClientController;
+use App\Http\Controllers\ProjectController;
 use App\Http\Controllers\SettingsController;
 
 Route::prefix('/dashboard')->name('dashboard')->middleware('auth')->group(function () {
@@ -21,6 +22,13 @@ Route::prefix('/dashboard')->name('dashboard')->middleware('auth')->group(functi
         Route::post('/', [ClientController::class, 'store'])->name('.store');
         Route::put('/{client}', [ClientController::class, 'update'])->name('.update');
         Route::delete('/{client}', [ClientController::class, 'destroy'])->name('.destroy');
+    });
+
+    Route::prefix('/projects')->name('.projects')->group(function () {
+        Route::get('/', [ProjectController::class, 'index']);
+        Route::post('/', [ProjectController::class, 'store'])->name('.store');
+        Route::put('/{project}', [ProjectController::class, 'update'])->name('.update');
+        Route::delete('/{project}', [ProjectController::class, 'destroy'])->name('.destroy');
     });
 });
 
