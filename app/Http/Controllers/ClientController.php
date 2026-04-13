c991f54475bb79c123cbd1e6730d86a50c83d67e
Enzo <digiovannienzo1@gmail.com>
2026-04-13 12:41:03 +0000

    Feat/clients

---
+++ app/Http/Controllers/ClientController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Account;
+use App\Models\Client;
+use Auth;
+use Illuminate\Http\Request;
+use Symfony\Component\Finder\Exception\AccessDeniedException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+class ClientController
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $clients = Client::all();
+
+        return view('dashboard.singletons.clients', [
+            'clients' => $clients,
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+
+    $user = Account::authenticated();
+
+
+        $client = $request->validate([
+            'name' => 'required|max:255',
+            'daily_rate' => 'required|between:0,100'
+        ]);
+
+        Client::create([
+            ...$client,
+            'user_id' => $user->id
+        ]);
+
+        return redirect()->route('dashboard.clients');
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Client $client)
+    {
+        $validated = $request->validate([
+            'name' => 'required|max:255',
+            'daily_rate' => 'required'
+        ]);
+
+        if ($client->user_id !== Account::authenticated()->id) {
+            throw new AccessDeniedException();
+        }
+
+        if (! Gate::allows('update', $client)) {
+            abort(403);
+        }
+
+        $client->update($validated);
+
+        return redirect()->route('dashboard.clients');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Client $client)
+    {
+        if ($client->user_id !== Account::authenticated()->id) {
+            throw new NotFoundHttpException();
+        }
+        $client->delete();
+
+        return to_route('dashboard.clients');    }
+}
