20ec7c7cc35e61f8b31e4812f53c5949bd2e55a7
Mathieu TUDISCO <mathieu@tudisco.fr>
2026-07-01 12:36:35 +0200

    📈 Analytics — pass user email as custom property to Plausible

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -33,7 +33,12 @@
     <script async src="https://e.mathieutu.dev/js/pa-R65tyUjkaNzke-vlTzJNJ.js"></script>
     <script>
         window.plausible=window.plausible||function(){(plausible.q=plausible.q||[]).push(arguments)},plausible.init=plausible.init||function(i){plausible.o=i||{}};
-        plausible.init()
+        plausible.init({
+            customProperties: {
+                @php($user = auth()->user())
+                userEmail: @json($user ? $user->email : null),
+            }
+        })
     </script>
     @endif
 </head>
